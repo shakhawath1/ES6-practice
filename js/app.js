d9a01eacfd3c4370e8f4b6794a6b2d2e3949ce14
shakhawath <m.shakhawath18@gmail.com>
2022-02-27 14:13:07 +0600

    arrow function practice

--- js/app.js
+++ js/app.js
@@ -15,9 +15,31 @@ const student = {
 
 // problem 1.
 // const lastName = student.name.last;
-student.study();
+// student.study();
 // problem 2.
 const mySattus = `
 I am astudent, my fevarit subject is ${student.subject[0]}, my id: ${student.id}, my name: ${student.name.title} ${student.name.first} ${student.name.last}
 `;
-console.log(mySattus);
+// console.log(mySattus);
+// problem 3.1
+const myNum = () => 89;
+// console.log(myNum());
+
+// 3.2
+const dividedBy7 = num => num / 7;
+const result = dividedBy7(70);
+// console.log(result);
+
+// 3.3
+const addAndDivide = (num1, num2) => (num1 + num2) / 2;
+const resule2 = addAndDivide(17, 37);
+// console.log(resule2);
+// 3.4 
+const addAndAdd = (num3, num4) => {
+    const num1 = num3 + 7;
+    const num2 = num4 + 7;
+    const total = num1 + num2;
+    return total;
+};
+const addBoth = addAndAdd(17, 91);
+// console.log(addBoth);
